e1fd384371d0b9fe55ccc7a927135995e118b5ec
arystankalmahanov <aarystan@outlook.com>
2021-03-11 19:02:33 +0600

    Added LoginActivity.kt Not Finished

---
+++ app/src/main/java/kz/arctan/endterm/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package kz.arctan.endterm.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
